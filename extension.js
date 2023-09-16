6358459e7ed96d196dda867e8f7c4e05b0369aa4
constneo <nqxkk@qq.com>
2023-09-16 19:24:41 +0800

    update

--- extension.js
+++ extension.js
@@ -3,24 +3,28 @@ const convert = require('./commands/convert')
 const pick = require('./commands/pick')
 const options = require('./commands/options')
 const message = require('./commands/message')
-const { selector, HelloCodeLens } = require('./codelens')
-const request = require('./commands/request')
 
 function activate(context) {
-  console.log('Congratulations, your extension  is now active!')
+  vscode.window.showInformationMessage('Convert Path extension  is now active!')
+
   context.subscriptions.push(convert)
   context.subscriptions.push(pick)
   context.subscriptions.push(options)
   context.subscriptions.push(message)
-  context.subscriptions.push(request)
 
-  // vscode.languages.registerCodeLensProvider(selector, new HelloCodeLens())
+  let disposable = vscode.commands.registerCommand('wi.getCursorPosition', () => {
+    const editor = vscode.window.activeTextEditor
+    if (editor) {
+      const position = editor.selection.active
+      const line = position.line
+      const column = position.character
 
-  // const p = vscode.workspace.workspaceFolders
-  // const rootPath = p && p.length > 0 ? p[0].uri.fsPath : undefined
-  // console.log(rootPath)
+      vscode.window.showInformationMessage(`当前光标位置：行 ${line + 1}，列 ${column + 1}`)
+    }
+  })
+
+  context.subscriptions.push(disposable)
 }
-
 function deactivate() {}
 
 module.exports = {
