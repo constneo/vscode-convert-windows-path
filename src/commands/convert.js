d6bed2daccb64683b332b06e7719b7fe8ed46297
constneo <nqxkk@qq.com>
2023-09-24 00:34:40 +0800

    update

--- src/commands/convert.js
+++ src/commands/convert.js
@@ -3,21 +3,18 @@ import * as vscode from "vscode";
 const { commands, window } = vscode;
 
 export default (context, command) => {
-	const disposable = commands.registerCommand("wi.convert", (arg) => {
+	const disposable = commands.registerCommand(command, (arg) => {
 		const { selection, edit, document } = window.activeTextEditor;
 
 		// env.clipboard.readText().then((text) => {
 		//     console.log("text:", text);
 		// });
 
-		console.log("selection:", selection);
 		const origin_path = document.getText(selection);
 		// /(^\")|(\"$)|\\/g
 		const finally_path = origin_path
 			.replace(/\\/g, "/")
 			.replace(/(^\")|(\"$)/g, "");
-
-		console.log("path:", finally_path);
 
 		edit((e) => {
 			e.replace(selection, finally_path);
